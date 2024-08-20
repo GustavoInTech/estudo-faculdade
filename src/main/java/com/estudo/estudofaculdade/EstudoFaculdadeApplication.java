902504b7b9079c657404ca5b13fb34fa5c3862d9
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-08-19 21:45:34 -0300

    adicionando paackege model e class Produto

---
+++ src/main/java/com/estudo/estudofaculdade/EstudoFaculdadeApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.estudofaculdade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoFaculdadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoFaculdadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudo/estudo_faculdade/EstudoFaculdadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.estudo_faculdade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoFaculdadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
