93e6dba5dea520422d7fd8e8acf84d0fda0f00fb
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-08-20 20:32:47 -0300

    adicionando o repositorio do produto

--- src/main/java/com/estudo/estudofaculdade/repository/ProdutoRepository.java
+++ src/main/java/com/estudo/estudofaculdade/repository/ProdutoRepository.java
@@ -1,7 +1,10 @@
 package com.estudo.estudofaculdade.repository;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
 import com.estudo.estudofaculdade.model.Produto;
 
@@ -18,5 +21,67 @@ public class ProdutoRepository {
      */
     public List<Produto> obteProdutos() {
         return produtos;
+    }
+
+    /**
+     * Metodo que retorna o produto encontrado pelo seu id.
+     * 
+     * @param id do produto que será localizado
+     * @return Retona um produto caso seja encontrado.
+     */
+    public Optional<Produto> obterPorId(Integer id) {
+        return produtos
+                .stream()
+                .filter(produto -> produto.getId() == id)
+                .findFirst();
+    }
+
+    /**
+     * Metodo para adicionar produto na lista.
+     * 
+     * @param produto que será adicionado.
+     * @return Retorna o produto que foi adicionado na lista.
+     */
+    public Produto adicionar(Produto produto) {
+
+        ultimoId++;
+
+        produto.setId(ultimoId);
+        produtos.add(produto);
+
+        return produto;
+    }
+
+    /**
+     * Metodo para deletar o produto por id.
+     * 
+     * @param id do produto a ser deletado.
+     */
+    public void deletar(Integer id) {
+        produtos.removeIf(produto -> produto.getId() == id);
+    }
+
+    /**
+     * Metodo para atualizar o produto na lista
+     * 
+     * @param produto que será atualizado
+     * @return Retorna o produto após atualizar a lista.
+     */
+    public Produto atualizar(Produto produto) {
+
+        // encontrar o produto na lista
+        Optional<Produto> produtoEncontrado = obterPorId(produto.getId());
+
+        if (produtoEncontrado.isEmpty()) {
+            throw new InputMismatchException("Prosuto não encontrado");
+        }
+
+        // Eu tenho que remover o produto antigo da lista.
+        deletar(produto.getId());
+
+        // Depois adicionar o produto atualizado na lista.
+        produtos.add(produto);
+
+        return produto;
     }
 }
