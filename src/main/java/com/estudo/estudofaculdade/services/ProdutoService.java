add2d4aa790c683c14057f967199556916005a20
Gustavo Rodrigues <gustavorodri.dev@gmail.com>
2024-08-21 21:25:38 -0300

    Adicionando controler do produto

--- src/main/java/com/estudo/estudofaculdade/services/ProdutoService.java
+++ src/main/java/com/estudo/estudofaculdade/services/ProdutoService.java
@@ -58,7 +58,8 @@ public class ProdutoService {
     /**
      * Metodo para atualizar o produto na lista
      * 
-     * @param produto que será atualizado
+     * @param produto que será atualizado.
+     * @param id      do produto.
      * @return Retorna o produto após atualizar a lista.
      */
     public Produto atualizar(Integer id, Produto produto) {
